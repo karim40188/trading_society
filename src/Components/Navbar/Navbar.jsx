909490c88e8d65047781207548736e79fc1bba4a
karim mohamed <karim40189@gmail.com>
2024-09-16 10:11:32 +0200

    edit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import logo from "../../assets/logo.png";
 import branding from "../../assets/branding_name.png";
 import { Link } from "react-router-dom";
@@ -16,46 +16,56 @@ function Navbar() {
     { name: "AR", path: "" },
   ]);
   return (
-    <Box
-      sx={{
-        display: "flex",
-        backgroundColor: "#303030",
-        alignItems: "center",
-        px: "6px",
-        py:"12px",
-        margin:'10px auto',
-        justifyContent:'space-around',
-        borderRadius:'15px',
-      }}
-    >
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Box
-          sx={{ maxWidth: "79px", maxHeight: "69px" }}
-          component="img"
-          src={logo}
-        />
-        <Box
-          sx={{ maxWidth: "175px", maxHeight: "38px" }}
-          component="img"
-          src={branding}
-        />
-      </Box>
-
+    <Container>
       <Box
-        component="ul"
-        sx={{ display: "flex", gap: "10px", alignItems: "center",flexWrap:'wrap' }}
+        sx={{
+          display: "flex",
+          backgroundColor: "#303030",
+          alignItems: "center",
+          px: "6px",
+          py: "12px",
+          margin: "10px auto",
+          justifyContent: "space-around",
+          borderRadius: "15px",
+       
+        
+        }}
       >
-        {links.map((link) => {
-          return (
-            <Box key={link.name} component="li">
-              <Link to={link.path}>
-                <Typography sx={{ color: "#fff" }}>{link.name}</Typography>
-              </Link>
-            </Box>
-          );
-        })}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box
+            sx={{ maxWidth: "79px", maxHeight: "69px" }}
+            component="img"
+            src={logo}
+          />
+          <Box
+            sx={{ maxWidth: "175px", maxHeight: "38px" }}
+            component="img"
+            src={branding}
+          />
+        </Box>
+
+        <Box
+          component="ul"
+          sx={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "center",
+            flexWrap: "wrap",
+            
+          }}
+        >
+          {links.map((link) => {
+            return (
+              <Box key={link.name} component="li">
+                <Link to={link.path}>
+                  <Typography sx={{ color: "#fff" }}>{link.name}</Typography>
+                </Link>
+              </Box>
+            );
+          })}
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 }
 
